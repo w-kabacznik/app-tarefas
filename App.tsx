42b82dca25697b1c89ab2613b2aa0f5fa104cd83
Willian Sousa Maia <w.kabacznik@hotmail.com>
2024-09-27 18:55:50 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
+import TopTabRoot from './src/navigation/TopTabRoot';
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <TopTabRoot />
+        </NavigationContainer>
+    );
+}
+
