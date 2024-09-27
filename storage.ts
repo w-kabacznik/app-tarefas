43ae2f375ff040cbb48ec2daf0d4d04030a2a03c
Willian Sousa Maia <w.kabacznik@hotmail.com>
2024-09-27 18:57:31 -0300

    storage.js

---
+++ storage.ts
@@ -0,0 +1,60 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+/**
+ * console.log('1 - 19:55:56')
+ * console.log('2 - 19:55:57')
+ * const response = await fetch('api')
+ * console.log('3 - 19:55:59')
+ * 
+ */
+
+
+// Salvando um valor
+// Uso: storeData('themePreference', 'dark')
+
+// async function storeData2() {
+// }
+
+export const storeData = async (key:string, value: any) => {
+    try {
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem(key, jsonValue);
+    } catch (e) {
+        console.error('Failed to save object', e);
+    }
+};
+
+// Lendo um valor
+// Uso: const themePreference = getData('themePreference')
+export const getData = async (key: string) => {
+    try {
+        const jsonValue = await AsyncStorage.getItem(key);
+        return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+        console.error('Failed to retrieve data', e);
+    }
+};
+
+// Remover um valor
+// Uso: removeData('themePreference')
+export const removeData = async (key: string) => {
+    try {
+        await AsyncStorage.removeItem(key);
+        console.log('Data removed');
+    } catch (e) {
+        // erro ao remover
+        console.error('Failed to remove data', e);
+    }
+};
+
+// Uso: clearData()
+export const clearData = async () => {
+    try {
+        await AsyncStorage.clear();
+        console.log('Data clear');
+    } catch (e) {
+        // erro ao remover
+        console.error('Failed to clear data', e);
+    }
+}
